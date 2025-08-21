391a764e614f88dbe9de92d74d2f78eba782fe5e
hkoti <145455886+hkoti@users.noreply.github.com>
2025-08-21 16:56:34 +0530

    Update standardCiCdPipeline.groovy

--- vars/standardCiCdPipeline.groovy
+++ vars/standardCiCdPipeline.groovy
@@ -20,24 +20,27 @@ def call(Map config) {
                     git url: 'https://github.com/hkoti/opentelemetry-demo.git', branch: 'main'
                 }
             }
-            stage('2. SonarQube Code Analysis') {
+                        stage('2. SonarQube Code Analysis') {
                 when { expression { return config.sonarProjectKey != null } }
                 steps {
                     script {
-                        def safeWorkspace = env.WORKSPACE.replaceAll('\\\\', '/')
-                        withCredentials([string(credentialsId: config.sonarCredentialId, variable: 'SONAR_LOGIN_TOKEN')]) {
-                            withSonarQubeEnv('sonar-server') {
-                                bat """
-                                    %DOCKER_EXE% run --rm --network=host ^
-                                    -v "${safeWorkspace}:/usr/src/app" ^
-                                    -w /usr/src/app/${config.sonarSourcesPath} ^
-                                    sonarsource/sonar-scanner-cli:latest ^
-                                    sonar-scanner ^
-                                    -D"sonar.projectKey=${config.sonarProjectKey}" ^
-                                    -D"sonar.sources=." ^
-                                    -D"sonar.host.url=http://localhost:9000" ^
-                                    -D"sonar.login=${SONAR_LOGIN_TOKEN}"
-                                """
+                        // --- THIS IS THE NEW LOGIC ---
+                        // Check if the project is a Gradle project
+                        if (config.buildTool == 'gradle') {
+                            // For Gradle, we run the gradlew command directly
+                            withCredentials([string(credentialsId: config.sonarCredentialId, variable: 'SONAR_LOGIN_TOKEN')]) {
+                                // We are running this inside the checkout directory, so relative paths are fine.
+                                bat "src\\ad\\gradlew.bat sonar -Dsonar.projectKey=${config.sonarProjectKey} -Dsonar.host.url=http://localhost:9000 -Dsonar.login=${SONAR_LOGIN_TOKEN}"
+                            }
+                        } else {
+                            // For all other project types, use the Docker-based scanner
+                            def safeWorkspace = env.WORKSPACE.replaceAll('\\\\', '/')
+                            withCredentials([string(credentialsId: config.sonarCredentialId, variable: 'SONAR_LOGIN_TOKEN')]) {
+                                withSonarQubeEnv('sonar-server') {
+                                    bat """
+                                        %DOCKER_EXE% run --rm --network=host -v "${safeWorkspace}:/usr/src/app" -w /usr/src/app/${config.sonarSourcesPath} sonarsource/sonar-scanner-cli:latest sonar-scanner -D"sonar.projectKey=${config.sonarProjectKey}" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=${SONAR_LOGIN_TOKEN}"
+                                    """
+                                }
                             }
                         }
                     }
