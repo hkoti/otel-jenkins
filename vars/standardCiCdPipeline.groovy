20e48b20c25319cf0af343f914edb2194c66b23f
hkoti <145455886+hkoti@users.noreply.github.com>
2025-08-21 17:23:59 +0530

    Update standardCiCdPipeline.groovy

--- vars/standardCiCdPipeline.groovy
+++ vars/standardCiCdPipeline.groovy
@@ -24,25 +24,23 @@ def call(Map config) {
                 when { expression { return config.sonarProjectKey != null } }
                 steps {
                     script {
-                        // We must handle Gradle as a special case
-                        if (config.buildTool == 'gradle') {
-                            withCredentials([string(credentialsId: config.sonarCredentialId, variable: 'SONAR_LOGIN_TOKEN')]) {
-                                // --- THIS IS THE FINAL FIX ---
-                                // 1. Use 'dir' to change into the correct subdirectory.
-                                // 2. Call gradlew from within that directory.
-                                dir('src/ad') {
-                                    bat "gradlew.bat sonar -Dsonar.projectKey=${config.sonarProjectKey} -Dsonar.host.url=http://localhost:9000 -Dsonar.login=${SONAR_LOGIN_TOKEN}"
-                                }
-                            }
-                        } else {
-                            // The logic for all other services remains the same.
-                            def safeWorkspace = env.WORKSPACE.replaceAll('\\\\', '/')
-                            withCredentials([string(credentialsId: config.sonarCredentialId, variable: 'SONAR_LOGIN_TOKEN')]) {
-                                withSonarQubeEnv('sonar-server') {
-                                    bat """
-                                        %DOCKER_EXE% run --rm --network=host -v "${safeWorkspace}:/usr/src/app" -w /usr/src/app/${config.sonarSourcesPath} sonarsource/sonar-scanner-cli:latest sonar-scanner -D"sonar.projectKey=${config.sonarProjectKey}" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=${SONAR_LOGIN_TOKEN}"
-                                    """
-                                }
+                        // --- THIS IS THE FINAL FIX ---
+                        // We will use the universal Docker-based scanner for ALL project types.
+                        // It is more reliable and does not require modifying the target project's build files.
+                        def safeWorkspace = env.WORKSPACE.replaceAll('\\\\', '/')
+                        withCredentials([string(credentialsId: config.sonarCredentialId, variable: 'SONAR_LOGIN_TOKEN')]) {
+                            withSonarQubeEnv('sonar-server') {
+                                bat """
+                                    %DOCKER_EXE% run --rm --network=host ^
+                                    -v "${safeWorkspace}:/usr/src/app" ^
+                                    -w /usr/src/app/${config.sonarSourcesPath} ^
+                                    sonarsource/sonar-scanner-cli:latest ^
+                                    sonar-scanner ^
+                                    -D"sonar.projectKey=${config.sonarProjectKey}" ^
+                                    -D"sonar.sources=." ^
+                                    -D"sonar.host.url=http://localhost:9000" ^
+                                    -D"sonar.login=${SONAR_LOGIN_TOKEN}"
+                                """
                             }
                         }
                     }
